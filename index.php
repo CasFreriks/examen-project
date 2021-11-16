3cc274046382adf44aa05247232339270fb5db90
Jordy krudde <jordykrudde1@gmail.com>
2021-11-16 16:20:23 +0100

    foto slider
    foto slider aan homepage toegevoegd

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/main.css">
 
     <link rel="stylesheet" href="https://mdbootstrap.com/snippets/styles.0839c4d26bba6648d360.css">
     <link
@@ -19,6 +20,29 @@
 <body>
     <?php include('shared/nav.php') ?>
 
+    <div class="container">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/tennisfoto1.jpg" class="d-block w-100" alt="tennisfoto1">
+                </div>
+            <div class="carousel-item">
+                    <img src="img/tennisfoto2.jpg" class="d-block w-100" alt="tennisfoto2">
+            </div>
+            <div class="carousel-item">
+                    <img src="img/tennisfoto3.jpg" class="d-block w-100" alt="tennisfoto3">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Vorige</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Volgende</span>
+        </button>
+        </div>
+    </div>
 
     <?php include('shared/footer.php') ?>
 </body>
