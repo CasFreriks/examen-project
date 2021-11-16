50cd50f34f40427f21b809642b6a7bd321b8a5df
CasFreriks <61309802+CasFreriks@users.noreply.github.com>
2021-11-16 13:44:48 +0100

    added dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -15,6 +15,9 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> <!-- Jquery for the mobile nav -->
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet"> <!-- Datatables bootstrapp 5-->
+    <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css" rel="stylesheet"> <!-- Datatables bootstrapp 5 theme-->
+
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> <!-- Google font Roboto -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet"> <!-- Google font Poppins -->
 </head>
@@ -48,27 +51,27 @@
     <div class="d-flex flex-column flex-shrink-0 p-3 bg-light sidebar">
         <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
-                <a href="#" class="nav-link active" aria-current="page">
-                    <i class='bx bxs-home me-2'></i>
+                <a href="#" class="nav-link active" style="width:100%" aria-current="page">
+                    <i class='bx bxs-group me-2'></i>
                     <span>Leden overzicht</span>
                 </a>
             </li>
             <li>
                 <a href="#" class="nav-link link-dark">
-                    <i class='bx bxs-dashboard me-2'></i>
+                    <i class='bx bxs-food-menu me-2'></i>
                     <span>Menukaart overzicht</span>
                 </a>
             </li>
             <li>
                 <a href="#" class="nav-link link-dark">
-                    <i class='bx bxs-group me-2'></i>
+                    <i class='bx bx-table me-2'></i>
                     <span>Toernooien overzicht</span>
                 </a>
             </li>
             <li>
                 <a href="#" class="nav-link link-dark">
-                    <i class='bx bxs-group me-2'></i>
-                    <span>stats</span>
+                    <i class='bx bxs-calendar-alt me-2'></i>
+                    <span>Baan reserveren</span>
                 </a>
             </li>
         </ul>
@@ -83,9 +86,36 @@
 
     <div class="content">
         <section class="contentSection">
-            <h5>Section 1</h5>
-
-            <p>This is a section, put whatever you want in here</p>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-10" style="margin:40px auto;">
+                        <table id="dataUserTable" class="table table-striped" style="width:100%">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Gebruikersnaam</th>
+                                <th>Email</th>
+                                <th>Rank</th>
+                                <th>Wijzigen</th>
+                                <th>Rank wijzigen</th>
+                                <th>Verwijderen</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>#</td>
+                                <td>cas freriks</td>
+                                <td>cas.freriks@student.graafschapcollege.nl</td>
+                                <td>Lid</td>
+                                <td><div class="btn btn-secondary">Wijzigen</div></td>
+                                <td><div class="btn btn-info">Rank wijzigen</div></td>
+                                <td><div class="btn btn-danger">Verwijderen</div></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 
@@ -96,7 +126,15 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
 <script src="js/mobileNav.js"></script>
+<script>
+    $(function (){
+        $('#dataUserTable').DataTable();
+    });
+
+</script>
 
 </body>
 </html>
