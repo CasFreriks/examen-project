d19bddc60f82f61282a2fcbbebe9d7427dbe914a
Jordy krudde <jordykrudde1@gmail.com>
2021-11-16 13:39:46 +0100

    footer added
    footer toegevoegd met include

--- shared/footer.php
+++ shared/footer.php
@@ -1,1 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/footer.css">
+    <title>Document</title>
+</head>
+<body>
+        <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Locatie</h3>
+                        <ul>
+                            <li>DoetinchemseStraat</li>
+                            <li>7001AB</li>
+                            <li>Doetinchem</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Contact</h3>
+                        <ul>
+                            <li>DTV@email.com</li>
+                            <li>0314 123456</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Doetinchemse tennisvereniging</h3>
+                        <p>DTV is een tennisvereniging in doetinchem waar je lekkero op je gemakje kan tennissen en squashen. Verder is dus stukje tekst vooral voor opvulling en staat hier niet zo veel interessants in. groetjes de goede developers van deze site</p>
+                    </div>
+                </div>
+                <p class="copyright">Doetinchemse tennisvereniging © 2021</p>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
+<body>
+
