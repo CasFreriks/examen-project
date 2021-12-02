ea9b658c335fff1a51a41dc1468c9808737ca4ec
CasFreriks <61309802+CasFreriks@users.noreply.github.com>
2021-12-02 12:49:00 +0100

    Update inschrijven-toernooi.php

--- actions/inschrijven-toernooi.php
+++ actions/inschrijven-toernooi.php
@@ -12,13 +12,13 @@ $lid_id = $_SESSION["lidID"];
 $select = $pdo->prepare("SELECT lid_id, toernooi_id FROM inschrijvingen WHERE lid_id = :lidID AND toernooi_id = :toernooiID");
 $select->bindParam(':lidID', $lid_id);
 $select->bindParam(':toernooiID', $toernooiID);
+$count = $select->rowCount();
 $select->execute();
 
 $select2 = $pdo->prepare("SELECT toernooi_id FROM inschrijvingen WHERE toernooi_id = :toernooiID");
 $select2->bindParam(':toernooiID', $toernooiID);
 $select2->execute();
 $toernooiCount = $select2->rowCount();
-$count = $select->rowCount();
 $data = $select->fetchAll(PDO::FETCH_ASSOC);
 
 $select3 = $pdo->prepare("SELECT toernooi_deelnemers FROM toernooien WHERE toernooi_id = :toernooiID");
@@ -29,8 +29,8 @@ $toernooiIncrease = $deelnemers['toernooi_deelnemers'] +=1;
 
 if($count < 1) {
     if($toernooiCount <= 31) {
-        $sth = $pdo->prepare("INSERT INTO inschrijvingen (lid_id, toernooi_id)values(:toernooiDeelnemers ,:toernooiID)");
-        $sth->bindParam(':toernooiDeelnemers', $toernooiIncrease);
+        $sth = $pdo->prepare("INSERT INTO inschrijvingen (lid_id, toernooi_id)values(:lid_id ,:toernooiID)");
+        $select->bindParam(':lidID', $lid_id);
         $sth->bindParam(':toernooiID', $toernooiID);
         $execute = $sth->execute();
 
