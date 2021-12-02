886c217ab9da064a6a99306a0de4e11cd144c032
CasFreriks <61309802+CasFreriks@users.noreply.github.com>
2021-12-02 12:44:51 +0100

    update inschrijven-toernooi / cedric

--- actions/inschrijven-toernooi.php
+++ actions/inschrijven-toernooi.php
@@ -24,17 +24,22 @@ $data = $select->fetchAll(PDO::FETCH_ASSOC);
 $select3 = $pdo->prepare("SELECT toernooi_deelnemers FROM toernooien WHERE toernooi_id = :toernooiID");
 $select3->bindParam(':toernooiID', $toernooiID);
 $select3->execute();
-$deelnemers = $select3->fetchAll(PDO::FETCH_ASSOC);
+$deelnemers = $select3->fetch();
 $toernooiIncrease = $deelnemers['toernooi_deelnemers'] +=1;
 
 if($count < 1) {
     if($toernooiCount <= 31) {
-        $sth = $pdo->prepare("INSERT INTO inschrijvingen (lid_id, toernooi_id)values($lid_id ,$toernooiID)");
-        $sth2 = $pdo->prepare("UPDATE toernooien SET toernooi_deelnemers = :toernooiDeelnemers WHERE toernooi_id = :toernooiID");
-        $sth2->bindParam(':toernooiDeelnemers', $toernooiIncrease);
-        $sth2->bindParam(':toernooiID', $toernooiID);
+        $sth = $pdo->prepare("INSERT INTO inschrijvingen (lid_id, toernooi_id)values(:toernooiDeelnemers ,:toernooiID)");
+        $sth->bindParam(':toernooiDeelnemers', $toernooiIncrease);
+        $sth->bindParam(':toernooiID', $toernooiID);
         $execute = $sth->execute();
+
+
+        $sth2 = $pdo->prepare("UPDATE toernooien SET toernooi_deelnemers = :toernooiDeelnemers WHERE toernooi_id = :toernooiID");
+        $sth2->bindParam(':toernooiID', $toernooiID);
+        $sth2->bindParam(':toernooiDeelnemers', $toernooiIncrease);
         $execute2 = $sth2->execute();
+
         $_SESSION["status"] = "Je bent ingeschreven voor dit toernooi!";
         $_SESSION["statusCode"] = "success";
         header("Location: ../inschrijven-toernooien.php");
@@ -47,5 +52,5 @@ if($count < 1) {
     $_SESSION["status"] = "Je bent al ingeschreven voor dit toernooi.";
     $_SESSION["statusCode"] = "error";
     header("Location: ../inschrijven-toernooien.php");
-}   
+}
 ?>
