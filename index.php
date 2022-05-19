4df6e07c4d89a981be2774cfb16ea0414e734108
Cas Freriks <freriks.cas@gmail.com>
2022-05-19 19:38:43 +0200

    added a new homepage

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 // hier start een session
-session_start() 
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
@@ -8,9 +8,9 @@ session_start()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/main.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -19,46 +19,80 @@ session_start()
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet"/>
 
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>  <!-- Custom icons from BoxIcons.com CSS -->
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>DTV | Home</title>
 </head>
 <body>
 <?php include('shared/nav.php') ?>
 
-<div class="hero-image">
-  <div class="hero-text">
-    <h1>Doetinchemse tennisvereniging</h1>
-    <p>tennisvereniging in doetinchem. supermooi en hier kan je erg fijn tennissen</p>
-      <?php if(isset($_SESSION["lidID"]) && $_SESSION["lidID"] != "") { ?>
-          <a class="heroBtn" href="actions/uitloggen.php">Log uit</a>
-      <?php } else { ?>
-          <a class="heroBtn" href="login.php">Log in</a>
-      <?php } ?>
-  </div>
+<div class="container">
+    <section class="hero-img">
+        <div class="hero-content">
+            <h1>Doetinchemse tennisvereniging</h1>
+
+            <p>tennisvereniging in doetinchem. supermooi en hier kan je erg fijn tennissen</p>
+
+            <?php if(isset($_SESSION["lidID"]) && $_SESSION["lidID"] != "") { ?>
+                <a href="actions/uitloggen.php">Log uit</a>
+            <?php } else { ?>
+                <a class="#" href="login.php">Log in</a>
+            <?php } ?>
+        </div>
+    </section>
+
+    <div class="row">
+        <div class="col-md-4">
+            <section class="info-box">
+                <img src="img/tennis.jpg" draggable="false" alt="tennis">
+
+                <h2>Tennis</h2>
+
+                <p>Bij de Doetinchemse tennisvereniging kunt u op 5 buiten banen tennisen.</p>
+            </section>
+        </div>
+        <div class="col-md-4">
+            <section class="info-box">
+                <img src="img/squashen.jpg" draggable="false" alt="squashen">
+
+                <h2>Squashen</h2>
+
+                <p>Bij de Doetinchemse tennisvereniging kunt u ook squashen en heeft u toegang tot 2 squashbanen.</p>
+            </section>
+        </div>
+        <div class="col-md-4">
+            <section class="info-box">
+                <img src="img/cafe.jpg" draggable="false" alt="cafe">
+
+                <h2>Restaurant</h2>
+
+                <p>Bij ons kan je na het sporten ook een hapje en een drankje doen.</p>
+            </section>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <section class="about-us">
+                <h2>Over ons</h2>
+
+                <p>
+                    Doetinchemse tennisvereniging is een grote gezellige tennis / squash vereniging met leuke toernooien
+                    waar je je op kan geven als je lid bent en een restaurant waar je ná of vóór het sporten even langs kan voor een
+                    gezellige sfeer en een lekker hapje en drankje.
+
+                    Doetinchemse tennisvereniging is zeer actief in het jeugdtennis en faciliteert hierin waar mogelijk.
+                    Ook organiseert DTV jaarlijks terugkerende evenementen met als hoogtepunt het bekende Doetinchemse
+                    Dubbel Open toernooi wat traditiegetrouw de laatste week van augustus wordt gehouden.
+                </p>
+
+            </section>
+        </div>
+    </div>
 </div>
 
-<div class="content">
-  <div class="row row-content">
-    <div class="col-lg-4 col-sm-6">
-      <h2>Tennissen op onze mooie buiten banen</h2>
-      <p>bij de Doetinchemse tennisvereniging kunt u op 5 buiten banen tennisen</p>
-    </div>
-    <div class="col-lg-8 col-sm-6">
-      <img class="article-image" src="img/tennisfoto2.jpg" alt="article-image">
-    </div>
-  </div>
 
-  <div class="row row-content">
-    <div class="col-lg-4 col-sm-6">
-      <h2>Squashen</h2>
-      <p>bij de Doetinchemse tennisvereniging kunt u ook squashen en heeft u toegang tot 2 squashbanen</p>
-    </div>
-    <div class="col-lg-8 col-sm-6">
-      <img class="article-image" src="img/tennisfoto3.jpg" alt="article-image">
-    </div>
-  </div>
-
-</div>
-    <?php include('shared/footer.php') ?>
+<?php include('shared/footer.php') ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
--- shared/footer.php
+++ shared/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
+                    <div class="col-sm-6 col-md-3 item footer-info">
                         <h3>Locatie</h3>
                         <ul>
                             <li>DoetinchemseStraat</li>
@@ -10,19 +10,21 @@
                             <li>Doetinchem</li>
                         </ul>
                     </div>
-                    <div class="col-sm-6 col-md-3 item">
+                    <div class="col-sm-6 col-md-3 item footer-info">
                         <h3>Contact</h3>
                         <ul>
                             <li>DTV@email.com</li>
                             <li>0314 123456</li>
                         </ul>
                     </div>
-                    <div class="col-md-6 item text">
+                    <div class="col-md-6 item footer-info">
                         <h3>Doetinchemse tennisvereniging</h3>
                         <p>DTV is een tennisvereniging in doetinchem waar je lekkero op je gemakje kan tennissen en squashen. Verder is dus stukje tekst vooral voor opvulling en staat hier niet zo veel interessants in. groetjes de goede developers van deze site</p>
                     </div>
                 </div>
-                <p class="copyright">Doetinchemse tennisvereniging © 2021</p>
+                <div class="copyright">
+                    <p>Doetinchemse tennisvereniging &copy;2021</p>
+                </div>
             </div>
         </footer>
     </div>
